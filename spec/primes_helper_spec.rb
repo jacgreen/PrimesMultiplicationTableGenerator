70ceec76c9f792192a7ba7b3778d636f27345111
Jack Green <jgreen@me.com>
2014-03-28 12:43:08 -0700

    spec to check the rind_primes_upper_bound method.

--- spec/primes_helper_spec.rb
+++ spec/primes_helper_spec.rb
@@ -1,10 +1,8 @@
-require 'rspec'
 require 'spec_helper'
+
 describe '#find_upper_bound' do
-
   it 'should return the integer upper bound given the desired number of primes' do
-
-    expect(find_upper_bound(1)).to eq(1)
-
+    expect(PrimesHelper.new().find_primes_upper_bound(5)).to eq(11)
+    expect(PrimesHelper.new().find_primes_upper_bound(6)).to eq(14)
   end
 end
