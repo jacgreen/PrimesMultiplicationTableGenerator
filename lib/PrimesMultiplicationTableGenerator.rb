8d9636e7f949456be5f809ac6cfaf8b4a31db1ae
Jack Green <jgreen@me.com>
2014-03-28 12:19:43 -0700

    killed pointless documentation.

--- lib/PrimesMultiplicationTableGenerator.rb
+++ lib/PrimesMultiplicationTableGenerator.rb
@@ -3,5 +3,4 @@ require 'PrimesMultiplicationTableGenerator/primes_helper.rb'
 require 'PrimesMultiplicationTableGenerator/table_generator.rb'
 
 module PrimesMultiplicationTableGenerator
-  # Your code goes here...
 end
