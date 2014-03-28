c9fa6a96a91f19b1bd2abf7f1cc994e60df87955
Jack Green <jgreen@me.com>
2014-03-28 13:47:02 -0700

    initial implementation of the table generator class.  mostly empty, but does figure out the max field width.

--- lib/PrimesMultiplicationTableGenerator/table_generator.rb
+++ lib/PrimesMultiplicationTableGenerator/table_generator.rb
@@ -1,2 +1,25 @@
 class TableGenerator
+
+  def initialize
+    @field_width = 1
+  end
+  def dump_table
+
+  end
+
+  def get_header_line
+
+  end
+
+  def get_line_contents
+
+  end
+
+  def field_width
+    return @field_width
+  end
+
+  def set_table_field_width(in_array_of_numbers)
+    @field_width = (in_array_of_numbers.last**2).to_s.length + 2
+  end
 end
