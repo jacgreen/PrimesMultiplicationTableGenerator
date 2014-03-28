f1a528c1be05a70ac39466f5c59a07632e045be1
Jack Green <jgreen@me.com>
2014-03-28 12:43:20 -0700

    implementation of the find_primes_upper_bound method.

--- lib/PrimesMultiplicationTableGenerator/primes_helper.rb
+++ lib/PrimesMultiplicationTableGenerator/primes_helper.rb
@@ -1,2 +1,14 @@
 class PrimesHelper
+
+  # a default method to find the upper bound for a set of prime numbers.
+  # subclasses may prefer to provide their own implementation.
+  def find_primes_upper_bound(in_number_of_primes)
+    if in_number_of_primes > 5
+      return (in_number_of_primes*Math.log(in_number_of_primes) + in_number_of_primes*Math.log( Math.log(in_number_of_primes) )).floor
+    else
+      # 11 is the 5th prime, no matter what number we're given, if it's
+      # less than 5, we're never gonna have an upper bound greater than 11.
+      return 11
+    end
+  end
 end
