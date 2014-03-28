3ffe16e1672e3269cd46d91f3df8cd0a811a0a65
Jack Green <jgreen@me.com>
2014-03-28 10:28:07 -0700

    initial empty classes, main module requires them.

--- lib/PrimesMultiplicationTableGenerator.rb
+++ lib/PrimesMultiplicationTableGenerator.rb
@@ -1,4 +1,6 @@
-require "PrimesMultiplicationTableGenerator/version"
+require 'PrimesMultiplicationTableGenerator/version'
+require 'PrimesMultiplicationTableGenerator/primes_helper.rb'
+require 'PrimesMultiplicationTableGenerator/table_generator.rb'
 
 module PrimesMultiplicationTableGenerator
   # Your code goes here...
---
+++ lib/PrimesMultiplicationTableGenerator/primes_helper.rb
@@ -0,0 +1,2 @@
+class PrimesHelper
+end
