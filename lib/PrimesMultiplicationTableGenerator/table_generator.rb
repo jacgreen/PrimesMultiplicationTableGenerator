08ca8dd1a8f23990309ecee0ed7ae0444c8bc0eb
Jack Green <jgreen@me.com>
2014-03-29 17:45:50 -0700

    initial implementation of the table generator module.

--- lib/PrimesMultiplicationTableGenerator/table_generator.rb
+++ lib/PrimesMultiplicationTableGenerator/table_generator.rb
@@ -1,25 +1,36 @@
-class TableGenerator
+module TableGenerator
 
-  def initialize
-    @field_width = 1
-  end
-  def dump_table
+  @field_width = 1
+  @format_string = "%#{@field_width}s\t"
+  class << self; attr_reader :field_width; attr_reader :format_string; end
 
-  end
+  def self.dump_table(in_array_of_numbers)
 
-  def get_header_line
-
-  end
-
-  def get_line_contents
+    puts (get_header_line_contents(in_array_of_numbers))
+    in_array_of_numbers.each { |prime|
+      puts (get_line_contents(prime, in_array_of_numbers))
+    }
 
   end
 
-  def field_width
-    return @field_width
+  def self.get_header_line_contents(in_array_of_numbers)
+    line = sprintf("%#{in_array_of_numbers.last.to_s.length + 1}s", '')
+    in_array_of_numbers.each { |number|
+      line = line + sprintf(@format_string, number)
+    }
+    line
   end
 
-  def set_table_field_width(in_array_of_numbers)
+  def self.get_line_contents(in_line_value, in_array_of_numbers)
+    line = sprintf("%#{in_array_of_numbers.last.to_s.length + 1}i", in_line_value)
+    in_array_of_numbers.each { |number|
+      line = line + sprintf(@format_string, in_line_value * number)
+    }
+    line
+  end
+
+  def self.set_table_field_width(in_array_of_numbers)
     @field_width = (in_array_of_numbers.last**2).to_s.length + 2
+    @format_string = "%#{@field_width}i"
   end
 end
