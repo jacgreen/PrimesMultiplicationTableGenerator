817d55d0735c660d68f769f2d0f73c83539b84b9
Jack Green <jgreen@me.com>
2014-03-29 17:45:26 -0700

    tests for the table generator class.

--- spec/table_generator_spec.rb
+++ spec/table_generator_spec.rb
@@ -1,10 +1,36 @@
 require 'spec_helper'
 
-describe '#set_table_field_width' do
+describe 'TableGenerator' do
 
-  it 'should set the field with member in the module' do
-    generator = TableGenerator.new()
-    generator.set_table_field_width([1,10])
-    expect(generator.field_width).to eq(5)
+  describe '#set_table_field_width' do
+    it 'should set the field with member in the module' do
+      TableGenerator.set_table_field_width([1,10])
+      expect(TableGenerator.field_width).to eq(5)
+    end
+  end
+ #    2   3   5
+ # 2  4   6  10
+ # 3  6   9  15
+ # 5 10  15  25
+
+  describe '#get_line_contents' do
+    it 'should build a line with the expected string' do
+      TableGenerator.set_table_field_width([1,10])
+      expect(TableGenerator.get_line_contents(2,[2,3,5])).to eq(" 2    4    6   10")
+    end
+  end
+
+  describe '#get_header_line' do
+    it 'should build a line with the expected header string' do
+      TableGenerator.set_table_field_width([1,10])
+      expect(TableGenerator.get_header_line_contents([2,3,5])).to eq("      2    3    5")
+    end
+  end
+
+  describe "#dump_table" do
+    it 'should build a string with expected contents' do
+      TableGenerator.set_table_field_width([1,10])
+      expect(TableGenerator.dump_table([2,3,5])).to eq("      2    3    5")
+    end
   end
 end
