9adb6b8cd87a2aee26fb6631e14eece4f49ea5b1
Jack Green <jgreen@me.com>
2014-03-30 20:12:56 -0700

    killed sample table.  fixed specs to expect a field at the beginning of the line that has the 'label' number in it.

--- spec/table_generator_spec.rb
+++ spec/table_generator_spec.rb
@@ -8,22 +8,18 @@ describe 'TableGenerator' do
       expect(TableGenerator.field_width).to eq(5)
     end
   end
- #    2   3   5
- # 2  4   6  10
- # 3  6   9  15
- # 5 10  15  25
 
   describe '#get_line_contents' do
     it 'should build a line with the expected string' do
       TableGenerator.prepare_table_field_width([1,10])
-      expect(TableGenerator.get_line_contents(2,[2,3,5])).to eq(" 2    4    6   10")
+      expect(TableGenerator.get_line_contents(2,[2,3,5])).to eq("    2    4    6   10")
     end
   end
 
   describe '#get_header_line' do
     it 'should build a line with the expected header string' do
       TableGenerator.prepare_table_field_width([1,10])
-      expect(TableGenerator.get_header_line_contents([2,3,5])).to eq("      2    3    5")
+      expect(TableGenerator.get_header_line_contents([2,3,5])).to eq("         2    3    5")
     end
   end
 
