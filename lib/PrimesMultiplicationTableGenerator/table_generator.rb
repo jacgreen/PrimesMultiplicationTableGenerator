523ca9e8290ac8e709a069d460c29ce94ef8625d
Jack Green <jgreen@me.com>
2014-03-31 09:23:15 -0700

    eliminated format_string variable.

--- lib/PrimesMultiplicationTableGenerator/table_generator.rb
+++ lib/PrimesMultiplicationTableGenerator/table_generator.rb
@@ -1,7 +1,6 @@
 module TableGenerator
 
-  @field_width = 1
-  @format_string = "%#{@field_width}s\t"
+  @field_width = 3
   class << self; attr_reader :field_width; attr_reader :format_string; end
 
   def self.dump_table(in_array_of_numbers)
@@ -10,13 +9,12 @@ module TableGenerator
     in_array_of_numbers.each { |prime|
       puts (get_line_contents(prime, in_array_of_numbers))
     }
-
   end
 
   def self.get_header_line_contents(in_array_of_numbers)
-    line = sprintf("%#{@field_width}s", ' ')
+    line = sprintf("%#{@field_width}s", '')
     in_array_of_numbers.each { |number|
-      line = line + sprintf(@format_string, number)
+      line = line + sprintf("%#{@field_width}i", number)
     }
     line
   end
@@ -24,13 +22,12 @@ module TableGenerator
   def self.get_line_contents(in_line_value, in_array_of_numbers)
     line = sprintf("%#{@field_width}i", in_line_value)
     in_array_of_numbers.each { |number|
-      line = line + sprintf(@format_string, in_line_value * number)
+      line = line + sprintf("%#{@field_width}i", in_line_value * number)
     }
     line
   end
 
   def self.prepare_table_field_width(in_array_of_numbers)
     @field_width = (in_array_of_numbers.last**2).to_s.length + 2
-    @format_string = "%#{@field_width}i"
   end
 end
