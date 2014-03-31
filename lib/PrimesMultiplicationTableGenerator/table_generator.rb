9cb70c4d302dc7799c2bca33c3a99149fdbd5339
Jack Green <jgreen@me.com>
2014-03-30 20:14:00 -0700

    dump_table was not preparing the table field width values.  fixed weird format strings to actually use the field_width value.

--- lib/PrimesMultiplicationTableGenerator/table_generator.rb
+++ lib/PrimesMultiplicationTableGenerator/table_generator.rb
@@ -5,7 +5,7 @@ module TableGenerator
   class << self; attr_reader :field_width; attr_reader :format_string; end
 
   def self.dump_table(in_array_of_numbers)
-
+    self.prepare_table_field_width(in_array_of_numbers)
     puts (get_header_line_contents(in_array_of_numbers))
     in_array_of_numbers.each { |prime|
       puts (get_line_contents(prime, in_array_of_numbers))
@@ -14,7 +14,7 @@ module TableGenerator
   end
 
   def self.get_header_line_contents(in_array_of_numbers)
-    line = sprintf("%#{in_array_of_numbers.last.to_s.length + 1}s", '')
+    line = sprintf("%#{@field_width}s", ' ')
     in_array_of_numbers.each { |number|
       line = line + sprintf(@format_string, number)
     }
@@ -22,7 +22,7 @@ module TableGenerator
   end
 
   def self.get_line_contents(in_line_value, in_array_of_numbers)
-    line = sprintf("%#{in_array_of_numbers.last.to_s.length + 1}i", in_line_value)
+    line = sprintf("%#{@field_width}i", in_line_value)
     in_array_of_numbers.each { |number|
       line = line + sprintf(@format_string, in_line_value * number)
     }
