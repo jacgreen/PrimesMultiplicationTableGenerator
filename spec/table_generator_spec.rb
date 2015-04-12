3202173f168b3bb7c089f1e85833f7d5d6053608
jack green <jack.green@g9ss.com>
2015-04-12 13:48:05 -0700

    fix broken spec

--- spec/table_generator_spec.rb
+++ spec/table_generator_spec.rb
@@ -2,31 +2,34 @@ require 'spec_helper'
 
 describe 'TableGenerator' do
 
-  describe '#set_table_field_width' do
-    it 'should set the field with member in the module' do
-      TableGenerator.prepare_table_field_width([1,10])
-      expect(TableGenerator.field_width).to eq(5)
+  before do
+    TableGenerator.prepare_table_field_width([1,10])
+  end
+
+  describe '.set_table_field_width' do
+    it 'sets the field with member in the module' do
+      expect(TableGenerator.field_width).to eql(5)
     end
   end
 
-  describe '#get_line_contents' do
-    it 'should build a line with the expected string' do
-      TableGenerator.prepare_table_field_width([1,10])
-      expect(TableGenerator.get_line_contents(2,[2,3,5])).to eq("    2    4    6   10")
+  describe '.get_line_contents' do
+    let(:line_contents) {'    2    4    6   10'}
+    it 'builds a line with the expected string' do
+      expect(TableGenerator.get_line_contents(2,[2,3,5])).to eql(line_contents)
     end
   end
 
-  describe '#get_header_line' do
-    it 'should build a line with the expected header string' do
-      TableGenerator.prepare_table_field_width([1,10])
-      expect(TableGenerator.get_header_line_contents([2,3,5])).to eq("         2    3    5")
+  describe '.get_header_line_contents' do
+    let(:header_line) {'         2    3    5'}
+    it 'builds a line with the expected header string' do
+      expect(TableGenerator.get_header_line_contents([2,3,5])).to eql(header_line)
     end
   end
 
-  describe "#dump_table" do
-    it 'should build a string with expected contents' do
-      TableGenerator.prepare_table_field_width([1,10])
-      expect(TableGenerator.dump_table([2,3,5])).to eq("      2    3    5")
+  describe '.dump_table' do
+    let(:table_contents) {/.*2.*3.*5\n.*2.*4.*6.*10\n.*3.*6.*9.*15\n.*5.*10.*15.*25/}
+    it 'builds a string with expected contents' do
+      expect{TableGenerator.dump_table([2,3,5])}.to output(table_contents).to_stdout
     end
   end
 end
