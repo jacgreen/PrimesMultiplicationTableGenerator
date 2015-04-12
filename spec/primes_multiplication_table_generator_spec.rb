ff48c74a9c0634788f1bfac7d4dec474de85b510
jack green <jack.green@g9ss.com>
2015-04-12 15:46:04 -0700

    improve whitespace / readability

--- spec/primes_multiplication_table_generator_spec.rb
+++ spec/primes_multiplication_table_generator_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe 'PrimesMultiplicationTableGenerator' do
+
   describe '.generate_multiplication_table' do
+
     let(:table_contents) {/.*2.*3.*5\n.*2.*4.*6.*10\n.*3.*6.*9.*15\n.*5.*10.*15.*25/}
+
     it 'builds a string with expected contents' do
       expect{PrimesMultiplicationTableGenerator.generate_multiplication_table(number_of_primes: 3)}.to output(table_contents).to_stdout
     end
